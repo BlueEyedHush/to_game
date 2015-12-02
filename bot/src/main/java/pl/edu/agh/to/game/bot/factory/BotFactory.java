337dc4046e0ab20fd2e7987e45981cc4a6a09b77
Damian Sepielak <damian.sepielak@gmail.com>
2015-12-02 19:26:46 +0100

    bot factory 'interface'

---
+++ bot/src/main/java/pl/edu/agh/to/game/bot/factory/BotFactory.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.to.game.bot.factory;
+
+import pl.edu.agh.to.game.common.Controller;
+
+public class BotFactory {
+    static public Controller createBot(String key){
+        return null;
+    }
+}
