fc8b960c72a72fe99d787f69e9b567a95d083fe7
Damian Sepielak <damian.sepielak@gmail.com>
2015-12-02 19:46:11 +0100

    boty - niestety niemozliwe do zaimplementowania po zmianach w interfejsach

---
+++ bot/src/main/java/pl/edu/agh/to/game/bot/SnakeBot.java
@@ -0,0 +1,18 @@
+package pl.edu.agh.to.game.bot;
+
+import pl.edu.agh.to.game.common.Controller;
+import pl.edu.agh.to.game.common.state.GameState;
+import pl.edu.agh.to.game.common.state.Vector;
+
+import java.util.List;
+
+/**
+ * Created by damian on 02.12.15.
+ */
+public class SnakeBot implements Controller {
+    @Override
+    public int makeMove(GameState gameState, int currentCarId, List<Vector> allowedPositions) {
+        //Vector currentPosition = gameState.getCarById(currentCarId).
+        return 0;
+    }
+}
