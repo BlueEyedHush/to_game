4c0d38ef51a778160cf43ae6947382ba2c48080f
mmajews <majewskimarcin@outlook.com>
2015-12-03 17:41:41 +0100

    -Creating new branch "client" -Adding sample windows for client -Main class for starting App and controller for handling gui

---
+++ client/src/main/java/pl/agh/to/game/client/Main.java
@@ -0,0 +1,28 @@
+package pl.agh.to.game.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("mainWindow.fxml"));
+        primaryStage.setTitle("THE GAME");
+        primaryStage.setMinHeight(500);
+        primaryStage.setMinWidth(650);
+        primaryStage.setWidth(650);
+        primaryStage.setHeight(500);
+        primaryStage.setScene(new Scene(root, 300, 275));
+
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ client/src/main/java/pl/agh/to/game/client/MainWindowController.java
@@ -0,0 +1,14 @@
+package pl.agh.to.game.client;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+
+public class MainWindowController {
+    @FXML
+    private Canvas canvas;
+
+    public void initialize() {
+        System.out.println("Test");
+    }
+
+}
+
