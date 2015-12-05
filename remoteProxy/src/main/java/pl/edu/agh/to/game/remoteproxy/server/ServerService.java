3202234e57bbb919f13fe2548af6712ef0eba0f5
pkruczek <piotrek628@gmail.com>
2015-12-05 21:25:56 +0100

    Interface fixes

--- remoteProxy/src/main/java/pl/edu/agh/to/game/remoteproxy/server/ServerService.java
+++ remoteProxy/src/main/java/pl/edu/agh/to/game/remoteproxy/server/ServerService.java
@@ -4,9 +4,10 @@ import java.util.Map;
 
 import pl.edu.agh.to.game.common.state.Board;
 import pl.edu.agh.to.game.common.state.CarState;
+import pl.edu.agh.to.game.remoteproxy.client.ClientService;
 
 public interface ServerService {
-	void handleConnect();
+	void handleConnect(ClientService service);
 	Board getBoard();
 	Map<Integer, CarState> getInitialCarStates();
 }
