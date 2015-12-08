92a28a0d5519858fc1ec892234e8fc85a8b287b4
Odyseja1602 <odyseja16@gmail.com>
2015-12-08 21:26:22 +0100

    test commit

--- server/src/main/java/pl/edu/agh/to/game/common/GameBuilder.java
+++ server/src/main/java/pl/edu/agh/to/game/common/GameBuilder.java
@@ -2,7 +2,7 @@ package pl.edu.agh.to.game.common;
 
 public class GameBuilder {
     public void registerController(Controller controller) {
-
+        //testing branches commit
     }
 
     public void registerObserver(Observer observer) {
