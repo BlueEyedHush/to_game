4a4d25380e715198b44165e7fd79ad4415d6c213
mmajews <majewskimarcin@outlook.com>
2015-12-09 14:42:39 +0100

    #refs client -Binding some actions to buttons

--- client/src/main/java/pl/agh/to/game/client/StartScreen.java
+++ client/src/main/java/pl/agh/to/game/client/StartScreen.java
@@ -2,20 +2,16 @@ package pl.agh.to.game.client;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import pl.edu.agh.to.game.common.state.Board;
@@ -44,45 +40,48 @@ public class StartScreen extends Application {
         primaryStage.setScene(theScene);
 
         //Creating game canvas
-        gameCanvas = new Canvas(1000,1000);
+        gameCanvas = new Canvas(1000, 1000);
 
         BorderPane border = new BorderPane();
         border.setPadding(new Insets(20, 0, 20, 20));
 
         HBox hButtons = new HBox();
         hButtons.setSpacing(10);
-        hButtons.setPadding(new Insets(20,20,10,20));
+        hButtons.setPadding(new Insets(20, 20, 10, 20));
 
-        Button button = new Button("Start game!");
+        Button startGameButton = new Button("Start game!");
+
+
+        startGameButton.setOnMouseClicked(event -> {
+            drawMap(gameCanvas, new GameState());
+        });
+
 
         Button buttonExit = new Button("Exit");
         buttonExit.setOnMouseClicked(event -> Platform.exit());
 
         Button buttonSettings = new Button("Settings");
 
-        hButtons.getChildren().addAll(button,buttonExit,buttonSettings);
+        hButtons.getChildren().addAll(startGameButton, buttonExit, buttonSettings);
 
         ScrollPane scrollPane = new ScrollPane(gameCanvas);
         scrollPane.setFitToWidth(true);
         scrollPane.setFitToHeight(true);
         scrollPane.setPrefSize(500, 400);
-        gridPane.addRow(0,hButtons);
-        gridPane.addRow(1,scrollPane);
+        gridPane.addRow(0, hButtons);
+        gridPane.addRow(1, scrollPane);
         root.getChildren().add(gridPane);
-
-
 
 
         GameState gameState = new GameState();
         Board board = new Board();
-//        this.drawMap(gameCanvas,gameState);
 
 
         primaryStage.show();
     }
 
 
-    private void drawMap(Canvas gameCanvas, GameState gameState){
+    private void drawMap(Canvas gameCanvas, GameState gameState) {
         //drawing only background with possible no go positions
 
         Board board = gameState.getBoard();
@@ -96,23 +95,22 @@ public class StartScreen extends Application {
 //        gameCanvas = new Canvas(mapSizeX*pointSize,mapSizeY*pointSize);
 
         GraphicsContext gc = gameCanvas.getGraphicsContext2D();
-        gc.fillRect(0,0,mapSizeX,mapSizeY);
+        gc.fillRect(0, 0, mapSizeX, mapSizeY);
 
-        for(int i= 0 ; i<mapSizeX;i++){
-            for(int j = 0 ; j<mapSizeY;j++){
-                boolean possible =  j%2==0;
-                if(possible){
+        for (int i = 0; i < mapSizeX; i++) {
+            for (int j = 0; j < mapSizeY; j++) {
+                boolean possible = j % 2 == 0;
+                if (possible) {
                     gc.setFill(Color.BISQUE);
-                }
-                else{
+                } else {
                     gc.setFill(Color.RED);
                 }
-                gc.fillRect(i*pointSize,j*pointSize,pointSize,pointSize);
+                gc.fillRect(i * pointSize, j * pointSize, pointSize, pointSize);
             }
         }
     }
 
-    public void run(){
+    public void run() {
 //        Thread thread = new Thread(Application::launch);
 //        thread.start();
         launch();
