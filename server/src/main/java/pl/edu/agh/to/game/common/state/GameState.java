70a97401949eddec8e6e1b88d9cbf3ac854de921
Odyseja1602 <odyseja16@gmail.com>
2015-12-09 23:24:38 +0100

    implemented GameState

--- server/src/main/java/pl/edu/agh/to/game/common/state/GameState.java
+++ server/src/main/java/pl/edu/agh/to/game/common/state/GameState.java
@@ -5,20 +5,45 @@ import java.util.Map;
 
 public class GameState {
     private Map<Integer, CarState> carStates;
+    private Board board;
+
+    GameState(Map<Integer, CarState> carStates, Board board) {
+        this.carStates = carStates;
+        this.board = board;
+    }
+
+    public void putCarState(int carId, CarState carState) {
+        carStates.put(carId, carState);
+    }
 
     public Board getBoard() {
-        return null;
+        return board;
     }
 
     public CarState getCarById(int id) {
-        return null;
-    }
-
-    public int getCurrentCarId() {
-        return 0;
+        return carStates.get(id);
     }
 
     public Map<Integer, CarState> getCarStates() {
-        return new Hashtable<Integer, CarState>(carStates);
+        return new Hashtable<>(carStates);
     }
+
+    public void changeCarState(int carId, CarState carState) {
+        carStates.replace(carId, carState);
+    }
+
+    //TODO keep occupied fields in Board?
+    public boolean validateField(Vector position) {
+        if (getBoard().get(position)) {
+            for (CarState carState : carStates.values()) {
+                if (carState.getPosition().equals(position)) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
