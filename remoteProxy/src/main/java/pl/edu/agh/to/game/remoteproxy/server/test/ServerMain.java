ac3326f67b099154646bf120871ea3177ff1902a
pkruczek <piotrek628@gmail.com>
2015-12-09 11:29:26 +0100

    Adding Lost classes

---
+++ remoteProxy/src/main/java/pl/edu/agh/to/game/remoteproxy/server/test/ServerMain.java
@@ -0,0 +1,51 @@
+package pl.edu.agh.to.game.remoteproxy.server.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import pl.edu.agh.to.game.common.Controller;
+import pl.edu.agh.to.game.common.GameBuilder;
+import pl.edu.agh.to.game.common.Observer;
+import pl.edu.agh.to.game.common.state.GameState;
+import pl.edu.agh.to.game.common.state.Vector;
+import pl.edu.agh.to.game.remoteproxy.server.RPServer;
+
+public class ServerMain {
+	public static void main(String[] args) {
+		GameBuilder builder = new TestGameBuilder(2, 2);
+		RPServer server = new RPServer(builder);
+		server.initialize(builder);
+		System.out.println("initialized");
+		TestGameBuilder testBuilder = (TestGameBuilder)builder;
+		List<Controller> controllers = testBuilder.getControllers();
+		List<Observer> observers = testBuilder.getObservers();
+		List<Vector> mockVectors = getMockVectors();
+		
+		for(Controller controller : controllers) {
+			controller.makeMove(new GameState(), 0, mockVectors);
+		}
+		
+	}
+	
+	public static List<Vector> getMockVectors() {
+		int n = 9;
+		Random rand = new Random();
+		
+		List<Vector> vectors = new ArrayList<>();
+		
+		System.out.println("Available moves");
+		
+		for(int i = 0; i < n; i++) {
+			Vector v = new Vector();
+			v.setX(rand.nextInt(10));
+			v.setY(rand.nextInt(10));
+			vectors.add(v);
+			
+			System.out.println("[" + v.getX() + ", " + v.getY() + "]");
+		}
+		
+		return vectors;
+	}
+	
+}
---
+++ remoteProxy/src/main/java/pl/edu/agh/to/game/remoteproxy/server/test/TestGameBuilder.java
@@ -0,0 +1,51 @@
+package pl.edu.agh.to.game.remoteproxy.server.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.edu.agh.to.game.common.Controller;
+import pl.edu.agh.to.game.common.GameBuilder;
+import pl.edu.agh.to.game.common.Observer;
+
+public class TestGameBuilder extends GameBuilder {
+	private List<Controller> controllers;
+	private List<Observer> observers;
+	private int requiredControllers;
+	private int requiredObservers;
+
+	public TestGameBuilder(int requiredControllers, int requiredObservers) {
+		this.controllers = new ArrayList<>();
+		this.observers = new ArrayList<>();
+		this.requiredControllers = requiredControllers;
+		this.requiredObservers = requiredObservers;
+	}
+	
+	public void registerController(Controller controller) {
+		controllers.add(controller);
+		System.out.println("Controller registered");
+	}
+	
+	public void registerObserver(Observer observer) {
+		observers.add(observer);
+		System.out.println("Observer registered");
+	}
+	
+	public int requiredControllers() {
+		return requiredControllers;
+	}
+	
+	public int requiredObservers() {
+		return requiredObservers;
+	}
+
+	public List<Controller> getControllers() {
+		return controllers;
+	}
+
+	public List<Observer> getObservers() {
+		return observers;
+	}
+	
+	
+	
+}
