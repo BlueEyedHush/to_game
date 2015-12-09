108cc91acf281d5c8eada192a005d04abed7b732
mixvar <mixvar@gmail.com>
2015-12-09 20:48:42 +0100

    Mock Server p1

--- remoteProxy/src/main/java/pl/edu/agh/to/game/remoteproxy/server/test/ServerMain.java
+++ remoteProxy/src/main/java/pl/edu/agh/to/game/remoteproxy/server/test/ServerMain.java
@@ -1,33 +1,120 @@
 package pl.edu.agh.to.game.remoteproxy.server.test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import pl.edu.agh.to.game.common.Controller;
 import pl.edu.agh.to.game.common.GameBuilder;
 import pl.edu.agh.to.game.common.Observer;
+import pl.edu.agh.to.game.common.Vector;
 import pl.edu.agh.to.game.common.state.GameState;
-import pl.edu.agh.to.game.common.state.Vector;
+import pl.edu.agh.to.game.remoteproxy.client.ClientType;
 import pl.edu.agh.to.game.remoteproxy.server.RPServer;
 
 public class ServerMain {
-	public static void main(String[] args) {
-		GameBuilder builder = new TestGameBuilder(2, 2);
+	private static Map<Integer, Controller> controllers;
+	private static List<Observer> observers;
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+
+
+		//.....................................
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		final String ERROR= "Invalid input, shutting down";
+
+		System.out.println("set required controllers:");
+		Integer reqControllers = Integer.parseInt(bufferedReader.readLine());
+		if(reqControllers<1){
+			System.out.println(ERROR);
+			return;
+		}
+		System.out.println("set required observers:");
+		Integer reqObservers = Integer.parseInt(bufferedReader.readLine());
+		if(reqObservers<0){
+			System.out.println(ERROR);
+			return;
+		}
+		GameBuilder builder = new TestGameBuilder(reqControllers, reqObservers);
+		
+		System.out.println("initializing...");
+
+		
 		RPServer server = new RPServer(builder);
 		server.initialize(builder);
-		System.out.println("initialized");
-		TestGameBuilder testBuilder = (TestGameBuilder)builder;
-		List<Controller> controllers = testBuilder.getControllers();
-		List<Observer> observers = testBuilder.getObservers();
-		List<Vector> mockVectors = getMockVectors();
 		
-		for(Controller controller : controllers) {
-			controller.makeMove(new GameState(), 0, mockVectors);
-		}
+		TestGameBuilder testBuilder = (TestGameBuilder)builder;
+		controllers = testBuilder.getControllers();
+		observers = testBuilder.getObservers();
+
+		System.out.println("initialized\t[OK]");
+		
+		while(true) {
+			System.out.println("\nServerConsole [? for help] > : ");
+			String cmd = bufferedReader.readLine();
+			
+			if(cmd.startsWith("?")) {
+				printHelp();
+			} else if(cmd.startsWith("players")) {
+				seePlayers();
+			} else if(cmd.startsWith("q")) {
+				break;
+			}
+		}		
+		return;
+	}
+	
+	
+	private static void printHelp() {
+		System.out.println("'?'           - print this help");
+		System.out.println("'players'     - displaye player list");
+		System.out.println("'q'           - close program");
 		
 	}
 	
+	private static void seePlayers(){
+		System.out.println("connected players: ");
+		for(int id:controllers.keySet())
+			System.out.println("controller "+id);
+		System.out.println();
+		for(int i=1;i<=observers.size()-controllers.size();i++){
+			System.out.println("observer "+i);
+		}
+		
+	}
+
+	public static List<Vector> getStandardVectors() {
+		List<Vector> vectors = new ArrayList<>();			
+			Vector v = new Vector(0,0);
+			vectors.add(v);
+			v = new Vector(0,1);
+			vectors.add(v);
+			v = new Vector(1,1);
+			vectors.add(v);
+			v = new Vector(1,0);
+			vectors.add(v);
+			v = new Vector(1,-1);
+			vectors.add(v);
+			v = new Vector(0,-1);
+			vectors.add(v);
+			v = new Vector(-1,-1);
+			vectors.add(v);
+			v = new Vector(-1,0);
+			vectors.add(v);
+			v = new Vector(-1,1);
+			vectors.add(v);			
+		return vectors;
+	}
+	
+
+	
+	/*
 	public static List<Vector> getMockVectors() {
 		int n = 9;
 		Random rand = new Random();
@@ -47,5 +134,5 @@ public class ServerMain {
 		
 		return vectors;
 	}
-	
+	*/
 }
