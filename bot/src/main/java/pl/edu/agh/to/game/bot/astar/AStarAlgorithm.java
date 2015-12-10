8d1fe93a88dba6ff8acfc742714c8bd049e50aa2
Damian Sepielak <damian.sepielak@gmail.com>
2015-12-10 02:38:02 +0100

    A star impl

---
+++ bot/src/main/java/pl/edu/agh/to/game/bot/astar/AStarAlgorithm.java
@@ -0,0 +1,61 @@
+package pl.edu.agh.to.game.bot.astar;
+
+import pl.edu.agh.to.game.common.state.Board;
+import pl.edu.agh.to.game.common.state.CarState;
+import pl.edu.agh.to.game.common.state.Vector;
+
+import java.util.*;
+
+public class AStarAlgorithm {
+    private static final Comparator<AStarNode> byGoalFuction =
+            ((o1, o2) -> Integer.compare(o1.getEstimatedGoalFunctionValue(), o2.getEstimatedGoalFunctionValue()));
+
+    private final Board board;
+
+    public AStarAlgorithm(Board board) {
+        this.board = board;
+    }
+
+    public Vector countNext(CarState initialCarState) {
+        AstarNodesGraph graph = new AstarNodesGraph(this.board, initialCarState.getPosition(), initialCarState.getVelocity());
+        Set<AStarNode> visited = new HashSet<>();
+        Set<AStarNode> toBeVisited = new HashSet<>();
+        AStarNode root = graph.getRoot();
+        root.setMovesTo(0);
+        toBeVisited.add(root);
+        while(!toBeVisited.isEmpty()) {
+            AStarNode x = toBeVisited.stream().sorted(byGoalFuction).findFirst().get();
+            if (isFinishNode(x)) {
+                return graph.getFirstMoveFromRoot(x);
+            }
+            visited.add(x);
+            toBeVisited.remove(x);
+            for (AStarNode neighbour : x.getNeighbours()) {
+                if(visited.contains(neighbour)) {
+                    continue;
+                }
+                int tentativeMovesTo = x.getMovesTo() + 1;
+                boolean tentativeIsBetter = false;
+                if (!toBeVisited.contains(neighbour)) {
+                    toBeVisited.add(neighbour);
+                    //h_score = estimate
+                    tentativeIsBetter = true;
+                } else if (tentativeMovesTo < neighbour.getMovesTo()) {
+                    tentativeIsBetter = true;
+                }
+                if (tentativeIsBetter) {
+                    neighbour.setCameFrom(x);
+                    neighbour.setMovesTo(x.getMovesTo() + 1);
+                    //f = g + h
+                }
+            }
+        }
+        throw new AStarFailureException();
+    }
+
+    private boolean isFinishNode(AStarNode node) {
+        return node.getPosition().equals(board.getFinish());
+    }
+
+
+}
---
+++ bot/src/main/java/pl/edu/agh/to/game/bot/astar/AStarNode.java
@@ -0,0 +1,87 @@
+package pl.edu.agh.to.game.bot.astar;
+
+import org.jetbrains.annotations.NotNull;
+import pl.edu.agh.to.game.common.state.Vector;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class AStarNode{
+
+    private final Vector position;
+    private final Vector velocity;
+    //can be deduced from velocity, but I believe it will simplify
+    private AStarNode cameFrom;
+    private Set<AStarNode> neighbours;
+    private AstarNodesGraph graph;
+    private AStarNode comeFrom;
+    private int movesTo;
+
+
+    public AStarNode(Vector position, Vector velocity, AstarNodesGraph graph) {
+        this.position = position;
+        this.velocity = velocity;
+        this.graph = graph;
+        movesTo = Integer.MAX_VALUE;
+    }
+
+    public Vector getPosition() {
+        return position;
+    }
+
+    public Vector getVelocity() {
+        return velocity;
+    }
+
+    public AStarNode getCameFrom() {
+        return cameFrom;
+    }
+
+    public void setComeFrom(AStarNode comeFrom) {
+        this.comeFrom = comeFrom;
+    }
+
+    public void setCameFrom(AStarNode cameFrom) {
+        this.cameFrom = cameFrom;
+    }
+
+    public void setMovesTo(int movesTo) {
+        this.movesTo = movesTo;
+    }
+
+    public Set<AStarNode> getNeighbours() {
+        if (this.neighbours == null) {
+            this.neighbours = graph.getNeigbours(this);
+        }
+        return Collections.unmodifiableSet(neighbours);
+    }
+
+    public int getMovesTo() {
+        return movesTo;
+    }
+
+    public int getEstimatedDstanceFromFinish() {
+        return graph.getDistanceFromFinishSquared(this);
+    }
+
+    public int getEstimatedGoalFunctionValue() {
+        return this.movesTo + getEstimatedDstanceFromFinish();
+    }
+
+    @Override
+    public int hashCode() {
+        return position.getX()*position.getY()*velocity.getX()*velocity.getY();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AStarNode)) return false;
+
+        AStarNode aStarNode = (AStarNode) o;
+
+        return position.equals(aStarNode.position) && velocity.equals(aStarNode.velocity);
+
+    }
+
+}
