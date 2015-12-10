1b98af9b5e80fd5c60b41e6409ccea8b379f3cd9
Damian Sepielak <damian.sepielak@gmail.com>
2015-12-10 02:37:16 +0100

    repackaging

--- bot/src/main/java/pl/edu/agh/to/game/bot/SnakeBot.java
+++ bot/src/main/java/pl/edu/agh/to/game/bot/SnakeBot.java
@@ -4,15 +4,38 @@ import pl.edu.agh.to.game.common.Controller;
 import pl.edu.agh.to.game.common.state.GameState;
 import pl.edu.agh.to.game.common.state.Vector;
 
+import java.util.Iterator;
 import java.util.List;
 
-/**
- * Created by damian on 02.12.15.
- */
 public class SnakeBot implements Controller {
-    @Override
-    public int makeMove(GameState gameState, int currentCarId, List<Vector> allowedPositions) {
-        //Vector currentPosition = gameState.getCarById(currentCarId).
+    public int makeMove(GameState gameState, int currentCarId, List<Vector> allowedPosition) {
+        Vector currentPosition = gameState.getCarById(currentCarId).getPosition();
+        Vector meta = gameState.getBoard().getFinish();
+
+        Vector move = new Vector();
+        move.setX(currentPosition.getX());
+        move.setY(currentPosition.getY());
+
+        if (currentPosition.getX() < meta.getX()) {
+            move.setX(move.getX() + 1);
+        } else if (currentPosition.getX() > meta.getX()) {
+            move.setX(move.getX() - 1);
+        }
+
+        if (currentPosition.getY() < meta.getY()) {
+            move.setY(move.getY() + 1);
+        } else if (currentPosition.getY() > meta.getY()) {
+            move.setY(move.getY() - 1);
+        }
+
+        int i = 0;
+        for (Iterator<Vector> it = allowedPosition.iterator(); it.hasNext(); i++) {
+            Vector v = it.next();
+            if(v.getX() == move.getX() && v.getY() == move.getY()) {
+                return i;
+            }
+        }
+
         return 0;
     }
 }
