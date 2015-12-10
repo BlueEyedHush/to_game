1f21ebc4939e3bfc8ee2113dbe40bf684235c039
pkruczek <piotrek628@gmail.com>
2015-12-10 02:26:40 +0100

    Before merge commit

--- remoteProxy/src/main/java/pl/edu/agh/to/game/remoteproxy/config/RemoteConfig.java
+++ remoteProxy/src/main/java/pl/edu/agh/to/game/remoteproxy/config/RemoteConfig.java
@@ -1,8 +1,5 @@
 package pl.edu.agh.to.game.remoteproxy.config;
 
 public class RemoteConfig {
-	public static final String RMI_ID = "RemoteProxy";
-	public static final int PORT = 22222;
-	public static final String SERVER_HOST = "192.168.0.2";
-	public static final String URL = "rmi://" + SERVER_HOST + "/" + RMI_ID;
+
 }
