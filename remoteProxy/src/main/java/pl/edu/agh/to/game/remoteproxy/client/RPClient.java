db3f195d3f84dddccdace9c08b40569e68864f16
pkruczek <piotrek628@gmail.com>
2015-12-16 19:56:30 +0100

    Client IP address set automatically

--- remoteProxy/src/main/java/pl/edu/agh/to/game/remoteproxy/client/RPClient.java
+++ remoteProxy/src/main/java/pl/edu/agh/to/game/remoteproxy/client/RPClient.java
@@ -1,10 +1,14 @@
 package pl.edu.agh.to.game.remoteproxy.client;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Enumeration;
 
 import pl.edu.agh.to.game.remoteproxy.config.RemoteConfig;
 import pl.edu.agh.to.game.remoteproxy.server.ServerService;
@@ -17,7 +21,7 @@ public class RPClient {
 	private ServerService server;
 
 	public RPClient(ClientActionHandler handler, ClientType type) throws RemoteException, NotBoundException {
-
+		setRmiHostname();
 		Registry registry = LocateRegistry.getRegistry(RemoteConfig.SERVER_HOST, RemoteConfig.PORT);
 		Remote lookup = registry.lookup(RemoteConfig.RMI_ID);
 		server = (ServerService) lookup;
@@ -26,5 +30,31 @@ public class RPClient {
 		this.client = new ClientRemoteObject(handler, type);
 		server.handleConnect(this.client);
 
+	}
+
+	private void setRmiHostname() {
+		// System.setProperty("java.net.preferIPv4Stack" , "true");
+		
+		try {
+			Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+			while (interfaces.hasMoreElements()) {
+				NetworkInterface iface = interfaces.nextElement();
+				// filters out 127.0.0.1 and inactive interfaces
+				if (iface.isLoopback() || !iface.isUp())
+					continue;
+
+				Enumeration<InetAddress> addresses = iface.getInetAddresses();
+				InetAddress addr = addresses.nextElement();
+				if (addresses.hasMoreElements()) {
+					System.out.println("Using " + addr.getHostAddress() + " as hostname");
+					System.setProperty("java.rmi.server.hostname", addr.getHostAddress());
+				} else {
+					throw new SocketException();
+				}
+
+			}
+		} catch (SocketException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
